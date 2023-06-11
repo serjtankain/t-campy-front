eafe7b3b5a9d6cde6859e493ffa1db12b175e158
ibrahim jridi <ibrahim.jridi@esprit.tn>
2023-06-11 20:56:09 +0100

    creating the project

---
+++ src/app/models/group.ts
@@ -0,0 +1,10 @@
+import { Activity } from "./activity"
+import { Offre } from "./offre"
+import { Reservation } from "./reservation"
+
+export class Group {
+  id!:number
+  activities!:Activity[]
+  offres!:Offre[]
+  reservations!:Reservation[]
+}
