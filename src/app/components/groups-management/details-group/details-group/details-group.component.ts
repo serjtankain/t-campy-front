07ac1710936c32b2cec756df7953582ecb2d3b0d
ibrahim jridi <ibrahim.jridi@esprit.tn>
2023-07-14 21:13:32 +0100

    modifying security,group-management and offre-management

--- src/app/components/groups-management/details-group/details-group/details-group.component.ts
+++ src/app/components/groups-management/details-group/details-group/details-group.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Group } from 'src/app/models/group';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GroupService } from 'src/app/services/group.service';
+import { ActivityService } from 'src/app/services/activity.service';
+import { Activity } from 'src/app/models/activity';
 
 @Component({
   selector: 'app-details-group',
@@ -11,9 +13,11 @@ import { GroupService } from 'src/app/services/group.service';
 export class DetailsGroupComponent implements OnInit {
   id!: number;
   group!: Group;
+  activityList: Activity[] = [];
 
   constructor(private route: ActivatedRoute,private router: Router,
-    private groupService: GroupService) { }
+    private groupService: GroupService,
+    private activityService:ActivityService) { }
 
   ngOnInit() {
     this.group = new Group();
@@ -25,11 +29,14 @@ export class DetailsGroupComponent implements OnInit {
         console.log(data)
         this.group = data;
       }, error => console.log(error));
+      this.getActivitiesByGroupId(this.id)
   }
 
   list(){
     this.router.navigate(['groups']);
   }
-
+  getActivitiesByGroupId(groupId: number){
+    this.activityService.getActivitiesByGroupId(groupId).subscribe(activities =>this.activityList=activities)
+  }
 
 }
